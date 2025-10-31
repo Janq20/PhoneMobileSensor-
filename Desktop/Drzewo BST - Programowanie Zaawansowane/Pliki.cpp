09fcd77c163bfd6899fab7437f5e928e68a7c80d
Janek W <89817308+Janq20@users.noreply.github.com>
2025-10-31 08:55:45 +0100

    Initial commit: Stworzenie struktury projektu i pustych plików klas

---
+++ Desktop/Drzewo BST - Programowanie Zaawansowane/Pliki.cpp
@@ -0,0 +1,1 @@
+#include "Pliki.h"
---
+++ Desktop/Drzewo BST - Programowanie Zaawansowane/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "DrzewoBST.h"
+#include "Pliki.h"
+
+using namespace std;
+
+int main() {
+    cout << "Start projektu Drzewo BST!" << endl;
+    return 0;
+}
